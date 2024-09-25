3ba409addf804ed67867c59927c4f9894a53dad5
nicolasmartinezwc <nicolasmartinezwc@gmail.com>
2024-09-25 00:51:39 -0300

    Added firebase and google sign in

--- BarrioConectado/BarrioConectadoApp.swift
+++ BarrioConectado/BarrioConectadoApp.swift
@@ -7,26 +7,34 @@
 
 import SwiftUI
 import FirebaseCore
+import GoogleSignIn
 
+// MARK: Firebase
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        AuthManager.instance.initializeStatus()
+        return true
+    }
+
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
 }
 
 @main
 struct BarrioConectadoApp: App {
-    // Register app delegate for Firebase setup
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
-    private var hasSession: Bool = true
-    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate // Registers the app delegate
+    @StateObject var authManager = AuthManager.instance
+
     var body: some Scene {
         WindowGroup {
             Group {
-                if hasSession {
+                if authManager.hasSession {
                     TabBarView()
                 } else {
                     StartView()
